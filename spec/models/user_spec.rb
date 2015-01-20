9a2ae8c54e8eabc6f39b5556571befa919863f2c
Luis Castro <luis@hashlabs.com>
2015-01-20 16:46:03 +0100

    Make a basic User model (including secure passwords)

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  let(:user) { User.new(name: "Example User", email: "user@example.com",
+                        password: "foobar", password_confirmation: "foobar") }
+
+  it "should be valid" do
+    expect(user).to be_valid
+  end
+
+  context "name" do
+    it "should be present" do
+      user.name = "     "
+      expect(user).to_not be_valid
+    end
+
+    it "should not be too long" do
+      user.name = "a" * 51
+      expect(user).to_not be_valid
+    end
+  end
+
+  context "email" do
+    it "should be present" do
+      user.email = "     "
+      expect(user).to_not be_valid
+    end
+
+    it "should not be too long" do
+      user.email = "a" * 244 + "@example.com"
+      expect(user).to_not be_valid
+    end
+
+    it "validation should accept valid addresses" do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        expect(user).to be_valid, "#{valid_address.inspect} should be valid"
+      end
+    end
+
+    it "validation should reject invalid addresses" do
+      invalid_addresses = %w[user@example,com user_at_foo.org
+                             user.name@example.foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+      end
+    end
+
+    it "addresses should be unique" do
+      duplicate_user = user.dup
+      duplicate_user.email = user.email.upcase
+      user.save
+      expect(duplicate_user).to_not be_valid
+    end
+  end
+
+  context "password" do
+    it "should have a minimum length" do
+      user.password = user.password_confirmation = "a" * 5
+      expect(user).to_not be_valid
+    end
+  end
+end
