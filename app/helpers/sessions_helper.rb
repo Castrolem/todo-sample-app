a8586c41513dfb14e4333db7c817fda9de1d0541
Luis Castro <luis@hashlabs.com>
2015-01-21 20:10:41 +0100

    Fix bugs and remember token problems

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,13 +5,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
@@ -42,5 +35,12 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 end
